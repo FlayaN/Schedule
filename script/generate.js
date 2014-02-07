8b1f02b4227458919d8c8ca3d03e2d3a4b58ae77
FlayaN <hannes.feldt@gmail.com>
2014-02-07 23:52:30 +0100

    Lots of updates, now calender works.

--- script/generate.js
+++ script/generate.js
@@ -2,162 +2,224 @@ var weekDays = ["M", "T", "O", "T", "F", "L", "S"];
 
 var months = ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
 
-var monthsDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-var year = 2014;
-
-var currMonth = 0;
-
-var currWeek = 0;
+var currDate = new Date();
 
 var currWeekDayStart = 0;
-var currWeekDayEnd = 1;
 
-var currDay = 1;
-var dayInMonth = 0;
+var currWeekDayEnd = 0;
+
+var currGroup = 1;
+
+Date.prototype.getWeek = function()
+{
+	var date = new Date(this.getTime());
+	date.setHours(0, 0, 0, 0);
+	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	var week1 = new Date(date.getFullYear(), 0, 4);
+	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+}
 
 window.onload = function()
 {
-	generateTest();
+	console.log(currDate.getDay());
+	
+	var min = new Date().getFullYear(),
+	max = min + 9;
+    
+    for(var i = min; i <= max; i++)
+	{
+		var opt = document.createElement('option');
+		opt.value = i;
+		opt.innerHTML = i;
+		document.getElementById('selectYear').appendChild(opt);
+	}
+	
+	for(var i = 1; i <= 4; i++)
+	{
+		var opt = document.createElement('option');
+		opt.value = i;
+		opt.innerHTML = i;
+		document.getElementById('group').appendChild(opt);
+	}
 }
 
 function generateTest()
 {
+	var e = document.getElementById('selectYear');
+	currDate.setFullYear(e.options[e.selectedIndex].value, 0, 1);
+	
+	e = document.getElementById('group');
+	currGroup = e.options[e.selectedIndex].value;
+	
+	currWeekDayEnd = currDate.getDay() == 0 ? 5 : currDate.getDay()-2;
+	
 	var module = document.createElement('div');
 	module.className = 'module';
-	currWeek++;
-	for(var i = 0; i < 12; i++)
-		module.appendChild(month(currMonth++));
-	document.getElementById('content').appendChild(module);
+	
+	var firstHalfyear = document.createElement('div');
+	firstHalfyear.className = 'inner';
+	
+	for(var i = 0; i < 6; i++)
+	{
+		firstHalfyear.appendChild(month());
+	}
+	
+	var secondHalfyear = document.createElement('div');
+	secondHalfyear.className = 'inner';
+	
+	for(var i = 0; i < 6; i++)
+	{
+		secondHalfyear.appendChild(month());
+	}
+	
+	module.appendChild(firstHalfyear);
+	module.appendChild(secondHalfyear);
+	
+	if(document.getElementById('content').childNodes.length == 0)
+		document.getElementById('content').appendChild(module);
+	else
+		document.getElementById('content').replaceChild(module, document.getElementById('content').childNodes[0]);
 }
 
-function month(monthNr)
+function month()
 {
 	var month = document.createElement('div');
 	month.className = 'month';
 	
 	var monthHeader = document.createElement('h1');
-	monthHeader.innerText = months[monthNr];
+	monthHeader.innerText = months[currDate.getMonth()];
 	
 	month.appendChild(monthHeader);
 	
+	var tmpDate = new Date();
+	tmpDate.setFullYear(currDate.getYear(), currDate.getMonth() + 1, 0);
+	var maxDays = tmpDate.getDate();
+	
 	var tmpCnt = 0;
-	for(var i = 0; i <= monthsDay[monthNr]/7; i++)
+	for(var i = 0; i <= maxDays/7; i++)
 	{
-		console.log("Month: " + months[monthNr] + " i: " + i + " monthsDay[monthNr]/7: " + Math.floor(monthsDay[monthNr]/7) + " monthsDay[monthNr]%7: " + monthsDay[monthNr]%7 + " monthsDay[monthNr]: " + monthsDay[monthNr] + " tmpCnt: " + tmpCnt);
-		if(i == Math.floor(monthsDay[monthNr]/7))
+		if(i == Math.floor(maxDays/7))
 		{
-			console.log("end");
 			currWeekDayStart = 0;
-			if(monthsDay[monthNr] - tmpCnt > 6)
+			if(maxDays - tmpCnt > 7)
 			{
-				console.log("end2");
-				var tmpWeek = week(currWeek++, currWeekDayStart, 6);
-				month.appendChild(tmpWeek);
-				currWeekDayEnd = 7 - (monthsDay[monthNr] - tmpCnt);
+				month.appendChild(week(0, 6));
+				tmpCnt += 7;
+				
+				currWeekDayEnd = maxDays - tmpCnt -1;
+				tmpCnt += currWeekDayEnd+1;
 			}
 			else
 			{
-				currWeekDayEnd = monthsDay[monthNr] - tmpCnt;
-				currWeek++;
+				currWeekDayEnd = maxDays - tmpCnt;
+				currWeekDayEnd--;
+				tmpCnt += currWeekDayEnd - currWeekDayStart;
 			}
 			
-			tmpCnt += currWeekDayEnd - currWeekDayStart;
-			currWeekDayEnd--;
+			
 		}
 		else if(i == 0 && currWeekDayEnd != 6)
 		{
-			console.log("start");
 			currWeekDayStart = currWeekDayEnd+1;
 			currWeekDayEnd = 6;
-			currDay = 1;
 			tmpCnt += currWeekDayEnd - currWeekDayStart + 1;
 		}
 		else
 		{
-			console.log("normal");
 			currWeekDayStart = 0;
 			currWeekDayEnd = 6;
 			tmpCnt += currWeekDayEnd - currWeekDayStart + 1;
-			currWeek++;
 		}
-		//tmpCnt += currWeekDayEnd - currWeekDayStart;
 		
-		var tmpWeek = week(currWeek, currWeekDayStart, currWeekDayEnd);
-		month.appendChild(tmpWeek);
+		month.appendChild(week(currWeekDayStart, currWeekDayEnd));
 	}
-	
-	/*month.appendChild(week(1, 0, 6));
-	month.appendChild(week(2, 0, 6));
-	month.appendChild(week(3, 0, 6));
-	month.appendChild(week(4, 0, 6));
-	month.appendChild(week(5, 0, 2));*/
-	
 	return month;
 }
 
-function week(weekNumber, start, stop)
+function week(start, stop)
 {
 	
 	var week = document.createElement('table');
-	week.className = 'week';
 	
-	if(currDay >= monthsDay[currMonth-1])
-		currDay = 1;
+	if(stop == 6)
+		week.className = 'week';
+	else
+		week.className = 'weekLast';
 	
 	for(var i = start; i <= stop; i++)
 	{
+		var tmpGroup = Math.floor(Math.random() * (4 - 1 + 1) + 1);
 		if(i == start)
-			week.appendChild(dayWithWeekNr(currDay++, weekDays[i], '1', weekNumber));
+			week.appendChild(dayWithWeekNr(weekDays[i], tmpGroup, tmpGroup, currDate.getWeek()));
 		else
-			week.appendChild(day(currDay++, weekDays[i], '1'));
+			week.appendChild(day(weekDays[i], tmpGroup, tmpGroup));
+			
+		currDate.setDate(currDate.getDate()+1);
 	}
-		
-	
 	return week;
 }
 
-function day(dateIn, weekDayIn, groupDayIn)
+function day(weekDayIn, groupDayIn, groupNightIn)
 {
 	var day = document.createElement('tr');
+	day.className = 'day';
 	
 	var date = document.createElement('td');
-	date.className = 'date';
-	date.innerText = dateIn.toString();
+	date.innerText = currDate.getDate().toString();
 	
 	var weekDay = document.createElement('td');
 	weekDay.innerText = weekDayIn;
 	
 	var groupDay = document.createElement('td');
 	groupDay.innerText = groupDayIn;
-	groupDay.className = 'green';
+	
+	if(groupDayIn == currGroup)
+		groupDay.className = 'green';
+	
+	var groupNight = document.createElement('td');
+	groupNight.innerText = groupNightIn;
+	
+	if(groupNightIn == currGroup)
+		groupNight.className = 'green';
 	
 	day.appendChild(date);
 	day.appendChild(weekDay);
 	day.appendChild(groupDay);
+	day.appendChild(groupNight);
 	return day;
 }
 
-function dayWithWeekNr(dateIn, weekDayIn, groupDayIn, weekNrIn)
+function dayWithWeekNr(weekDayIn, groupDayIn, groupNightIn, weekNrIn)
 {
 	var day = document.createElement('tr');
+	day.className = 'day';
 	
 	var date = document.createElement('td');
-	date.innerText = dateIn.toString();
+	date.innerText = currDate.getDate().toString();
 	
 	var weekDay = document.createElement('td');
 	weekDay.innerText = weekDayIn;
 	
 	var groupDay = document.createElement('td');
 	groupDay.innerText = groupDayIn;
-	groupDay.className = 'green';
+	
+	if(groupDayIn == currGroup)
+		groupDay.className = 'green';
+	
+	var groupNight = document.createElement('td');
+	groupNight.innerText = groupNightIn;
+	
+	if(groupNightIn == currGroup)
+		groupNight.className = 'green';
 	
 	var weekNr = document.createElement('td');
+	weekNr.className = 'weekNumber';
 	weekNr.innerText = weekNrIn.toString();
 	
 	day.appendChild(date);
 	day.appendChild(weekDay);
 	day.appendChild(groupDay);
+	day.appendChild(groupNight);
 	day.appendChild(weekNr);
 	return day;
 }
